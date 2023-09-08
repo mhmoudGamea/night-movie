ebda4ea054298138cd308f0f43b49260daa6b3b1
Mhmoud gamea <mhmoudgamea@gmail.com>
2023-09-08 11:36:45 +0300

    enhance ui loading with shimmer

---
+++ lib/core/widgets/shimmer_list_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerListView extends StatelessWidget {
+  const ShimmerListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[850]!,
+      highlightColor: Colors.grey[800]!,
+      child: SizedBox(
+        height: 150,
+        child: ListView.separated(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemCount: 20,
+          separatorBuilder: (context, index) => const SizedBox(width: 10),
+          itemBuilder: (context, index) => Container(
+            width: 97.5,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
